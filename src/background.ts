d2ccbe1f60ebd284bb38282616491821e2061c21
yu fujioka <fujiokayu@gmail.com>
2023-10-29 21:11:57 +0900

    add recording toggle feature

--- src/background.ts
+++ src/background.ts
@@ -1,45 +1,42 @@
 import { saveLog, exportLogs } from './logUtils';
 
+let isRecording = false;
 let currentFilter = "";
 
-chrome.webRequest.onBeforeRequest.addListener(
-  (details: chrome.webRequest.WebRequestBodyDetails) => {
-    if (currentFilter && !details.url.includes(currentFilter)) {
-      return; // フィルタに一致しないURLの場合、処理を終了
+const logRequest = (details: chrome.webRequest.WebRequestBodyDetails) => {
+  if (currentFilter && !details.url.includes(currentFilter)) {
+    return;
+  }
+  console.log("Request Method:", details.method);
+  console.log("Request URL:", details.url);
+
+  if (details.requestBody) {
+    console.log("requestBody:");
+    const requestBody = details.requestBody;
+
+    // formData
+    if (requestBody.formData) {
+      console.log("Request Body (form data):", requestBody.formData);
     }
-    console.log("Request Method:", details.method);
-    console.log("Request URL:", details.url);
 
-    if (details.requestBody) {
-      console.log("requestBody:");
-      const requestBody = details.requestBody;
-
-      // formDataの場合
-      if (requestBody.formData) {
-        console.log("Request Body (form data):", requestBody.formData);
-      }
-
-      // rawの場合 (通常のPOSTデータ)
-      else if (requestBody.raw) {
-        requestBody.raw.forEach((rawBytes) => {
-          if (rawBytes.bytes) {
-            const byteArray = new Uint8Array(rawBytes.bytes);
-            const decodedString = String.fromCharCode(...byteArray);
-            console.log("Request Body (raw):", decodedString);
-          }
-        });
-      }
+    // raw data
+    else if (requestBody.raw) {
+      requestBody.raw.forEach((rawBytes) => {
+        if (rawBytes.bytes) {
+          const byteArray = new Uint8Array(rawBytes.bytes);
+          const decodedString = String.fromCharCode(...byteArray);
+          console.log("Request Body (raw):", decodedString);
+        }
+      });
     }
-    const logEntry = `
-      Request Method: ${details.method}
-      Request URL: ${details.url}
-      Request Body: ${details.requestBody ? JSON.stringify(details.requestBody) : 'N/A'}
-    `;
-    saveLog(logEntry);
-  },
-  { urls: ["<all_urls>"] },
-  ["requestBody"]
-);
+  }
+  const logEntry = `
+    Request Method: ${details.method}
+    Request URL: ${details.url}
+    Request Body: ${details.requestBody ? JSON.stringify(details.requestBody) : 'N/A'}
+  `;
+  saveLog(logEntry);
+};
 
 chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
   if (message.action === 'setFilter') {
@@ -48,5 +45,28 @@ chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
     sendResponse({ message: "Filter set successfully!" });
   } else if (message.action === 'exportLogs') {
     exportLogs();
+  } else if (message.action === "toggleRecording") {
+    isRecording = !isRecording;
+
+    if (isRecording) {
+      // start to record log
+      chrome.webRequest.onBeforeRequest.addListener(
+        logRequest,
+        { urls: ["<all_urls>"] },
+        ["requestBody"]
+      );
+    } else {
+      // stop recording
+      chrome.webRequest.onBeforeRequest.removeListener(logRequest);
+    }
+
+    sendResponse({ isRecording: isRecording });
+  } else if (message.action === 'getRecordingState') {
+    sendResponse({ isRecording: isRecording });
   }
+});
+
+// Start with logging disabled when the extension is loaded.
+chrome.runtime.onInstalled.addListener(() => {
+  isRecording = false;
 });
--- src/popup.ts
+++ src/popup.ts
@@ -1,3 +1,18 @@
+// add listener to get recording toggle switch
+const recordingToggleSwitch = document.getElementById('recordingToggle') as HTMLInputElement;
+
+recordingToggleSwitch.addEventListener('change', () => {
+  chrome.runtime.sendMessage({ action: 'toggleRecording' }, response => {
+    recordingToggleSwitch.checked = response.isRecording;
+  });
+});
+
+// getRecordingState
+chrome.runtime.sendMessage({ action: 'getRecordingState' }, response => {
+  recordingToggleSwitch.checked = response.isRecording;
+});
+
+// add listener to save filter keywords
 document.getElementById('save-filter-settings')?.addEventListener('click', () => {
   const pathFilter = (document.getElementById('pathFilter') as HTMLInputElement).value;
 
@@ -9,10 +24,12 @@ document.getElementById('save-filter-settings')?.addEventListener('click', () =>
   });
 });
 
+// add listener to export log
 document.getElementById('exportButton')?.addEventListener('click', () => {
   chrome.runtime.sendMessage({ action: 'exportLogs' });
 });
 
+// add listener to clear storage
 document.getElementById('clearStorage')?.addEventListener('click', () => {
   chrome.storage.local.clear(() => {
       const error = chrome.runtime.lastError;
